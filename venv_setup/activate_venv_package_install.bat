bd8fc2d11915e8cb10a48676471b3fbce665fcd3
aceh40 <assenuma@yahoo.com>
2018-01-28 21:35:37 -0500

    fixed venv

--- venv_setup/activate_venv_package_install.bat
+++ venv_setup/activate_venv_package_install.bat
@@ -1,5 +1,5 @@
 @echo on 
 
-cmd /k "%userprofile%\Documents\GitHub\Flask_MyFirstApp\app\ve\scripts\activate & cd C:\Users\assen_bankov\Documents\GitHub\Flask_MyFirstApp\venv_setup & package_installs.bat"
+cmd /k "%userprofile%\PycharmProjects\Flask_MyFirstApp\app\venv\scripts\activate & cd C:\Users\assen_bankov\PycharmProjects\Flask_MyFirstApp\venv_setup & package_installs.bat"
 
 
